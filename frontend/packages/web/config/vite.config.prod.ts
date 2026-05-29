da1e326d9cd48a16a45ffcb8d3660a8b246f0b8b
wyhubuntu <wyhubuntu@example.com>
2026-05-29 16:18:49 +0800

    fix: remove broken @vitejs/plugin-legacy

--- frontend/packages/web/config/vite.config.prod.ts
+++ frontend/packages/web/config/vite.config.prod.ts
@@ -1,7 +1,6 @@
 import configCompressPlugin from './plugin/compress';
 import configVisualizerPlugin from './plugin/visualizer';
 import baseConfig from './vite.config.base';
-import legacy from '@vitejs/plugin-legacy';
 import { mergeConfig } from 'vite';
 
 export default mergeConfig(
@@ -10,10 +9,6 @@ export default mergeConfig(
     plugins: [
       configCompressPlugin('gzip'),
       configVisualizerPlugin(),
-      // 兼容性配置，配合package.json中的browserslist使用
-      legacy({
-        targets: ['defaults', 'not IE 11'],
-      }),
     ],
     build: {
       rollupOptions: {
